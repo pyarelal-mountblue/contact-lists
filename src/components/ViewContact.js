5d4e1df98222e4e3c3b9f1c954e02f19f78f1365
pyarelal-mountblue <pyarelal.sahu.17@mountblue.tech>
2021-09-12 14:45:03 +0530

    ViewContact.js added

---
+++ src/components/ViewContact.js
@@ -0,0 +1,90 @@
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@material-ui/core";
+import React from "react";
+
+const ViewContact = ({ contactList, removeContact, editContact }) => {
+  let serialNumber = 0;
+
+  contactList = contactList.sort((firstRow, secondRow) =>
+    firstRow.name > secondRow.name ? 1 : secondRow.name > firstRow.name ? -1 : 0
+  );
+
+  return (
+    <>
+      <h1 className="text">
+        <b>
+          <u>CONTACT LISTS</u>
+        </b>
+      </h1>
+      {contactList.length > 0 ? (
+        <Table>
+          <TableHead>
+            <TableCell></TableCell>
+            <TableCell>
+              <h2>
+                <b>Serial No.</b>
+              </h2>
+            </TableCell>
+            <TableCell>
+              <h2>
+                <b>Name</b>
+              </h2>
+            </TableCell>
+            <TableCell>
+              <h2>
+                <b>Mobile No.</b>
+              </h2>
+            </TableCell>
+            <TableCell>
+              <h2>
+                <b>Email</b>
+              </h2>
+            </TableCell>
+          </TableHead>
+          <TableBody>
+            {contactList.map((contact) => {
+              const { id, name, number, email } = contact;
+              serialNumber += 1;
+              return (
+                <>
+                  <TableRow key={id} className="table">
+                    <TableCell></TableCell>
+                    <TableCell>{serialNumber}</TableCell>
+                    <TableCell>{name}</TableCell>
+                    <TableCell>{number}</TableCell>
+                    <TableCell>{email}</TableCell>
+                    <TableCell>
+                      <button
+                        type="button"
+                        className="btnYellow"
+                        onClick={() => editContact(contact)}
+                      >
+                        Edit
+                      </button>
+                      <button
+                        type="button"
+                        className="btnRed"
+                        onClick={() => removeContact(id)}
+                      >
+                        Remove
+                      </button>
+                    </TableCell>
+                  </TableRow>
+                </>
+              );
+            })}
+          </TableBody>
+        </Table>
+      ) : (
+        <h2 className="text">Contact list is empty Please add a new contact</h2>
+      )}
+    </>
+  );
+};
+
+export default ViewContact;
